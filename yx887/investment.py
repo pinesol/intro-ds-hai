1d4b7d7fc1fca74d7ab025ab31633c2f090c9706
yx887 <ryjxiao@nyu.edu>
2014-11-05 19:38:53 -0500

    first commit

---
+++ yx887/investment.py
@@ -0,0 +1,34 @@
+from __future__ import division
+import numpy as np
+
+class Invest(object):
+    """ Investment instrument class """
+    def __init__(self, asset):
+        """ Constructor of the class. 
+
+        Args:
+            asset (int): Total asset possessed. 
+
+        """
+        self.asset = asset
+
+    def simulate(self, ntrials, position):
+        """ Simulate the investment given number of shares to purchase.
+
+        Args:
+            ntrials (int): number of trials to simulate.
+            position (int): number of shares to purchase.
+
+        Returns:
+            (float): daily return rate calculated from the simulation results.
+
+        """
+        position_value = self.asset / position    # Value per share given position
+        cumu_ret = []
+        for trial in xrange(ntrials):
+            cumu_ret.append(sum(np.random.uniform(size=position)>0.49) * position_value * 2)
+
+        daily_ret = [ret/1000-1 for ret in cumu_ret]
+        return daily_ret
+
+    
