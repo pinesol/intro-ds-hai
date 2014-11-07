d8dfc74715fe8aa4d0eaa6a31af38949607b9573
Lucy Wang <lw1582@nyu.edu>
2014-11-06 23:50:21 -0500

    assignment8

---
+++ lw1582/Investment.py
@@ -0,0 +1,57 @@
+'''import libraries used for calculations'''
+import numpy as np
+
+def investment(positions, num_trials):
+  
+  '''this program accepts two inputs: positions and num_trials:
+     positions: list of number of shares to buy in parallel
+     num_trials: how many times to randomly repeat the test
+
+     the program returns the outcomes of num_trials times'''
+  final_ret = np.zeros((num_trials,1))
+  for position in positions:
+    position_value = 1000 / position
+    
+    win = position_value * 2
+    odds = np.random.rand(num_trials,position)
+    np.place(odds, odds > 0.51, 0)
+    np.place(odds, odds != 0, win)
+    cumu_ret = np.sum(odds, axis = 1)
+    daily_ret = cumu_ret / 1000 -1
+    daily_ret = np.expand_dims(daily_ret, axis =1)
+    final_ret = np.concatenate((final_ret, daily_ret),axis = 1)
+    
+  final_ret = np.delete(final_ret,0,1)
+  return final_ret
+    
+def main():
+  positions = [1,10,100,1000]
+  num_trials = 10000
+
+  result = investment(positions,num_trials)
+  
+  print result
+  print result.shape
+if __name__ == "__main__":
+  main()
+  
+
+'''
+    #iwhile trial <  num_trials:
+     # position_to_bet = 0
+      while position_to_bet < position:
+        odd_today = numpy.random.rand().
+
+        if odd_today > 0.51:
+          ret[position_to_bet] = position_value * 2
+        else:
+          ret[position_to_bet] = 0
+
+      cumu_ret[trial] = np.sum(ret)
+
+      daily_ret[trial] = (cumu_ret[trial]/1000) - 1
+      
+      trial +=1
+
+    positions_daily_return.append(daily_ret)
+'''
---
+++ lw1582/result.py
@@ -0,0 +1,38 @@
+from Investment import investment
+import numpy as np
+from matplotlib import pyplot as plt
+
+def result(positions, num_trials):
+  final_result = investment(positions,num_trials)
+  
+  ret_mean = np.mean(final_result,axis = 0)
+  ret_std = np.std(final_result,axis = 0)
+  
+  f = open("results.txt","w")
+  
+  i = 0
+
+  while i < len(positions):
+    f.write("position: " + str(positions[i]) + "\nmean: " + str(ret_mean[i]) + "\nstd: " + str(ret_std[i]) + "\n")
+    i = i + 1
+
+  f.close()
+
+  i = 0
+
+  while i < len(positions):
+    plt.figure()
+    plt.hist(final_result[:,i],100,range=[-1,1])
+    plt.savefig("histogram_{0:04}_pos.pdf".format(positions[i]))
+    i +=1
+  return "completed"
+
+def main():
+  positions = [1,10,100,1000]
+  num_trials = 10000
+
+  result(positions, num_trials)
+
+if __name__ == "__main__":
+  main()
+
