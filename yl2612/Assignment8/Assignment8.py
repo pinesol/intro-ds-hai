a572544959d2e2b52688d4f817170e2b40f5c345
Kayla Li <yl2612@nyu.edu>
2014-11-08 19:10:12 -0500

    assignment 8

---
+++ yl2612/Assignment8/Assignment8.py
@@ -0,0 +1,30 @@
+from simulation import simulation
+import matplotlib.pyplot as plt
+import numpy as np
+
+def main():
+    '''This program stimulate each position, plot the results, store the means and standard deviations in a text file.'''
+    position = [1, 10, 100, 1000]
+    num_trials = 10000
+    results = open('results.txt','w') #open a file to store mean and standard deviation
+    
+    for position in position:
+        daily_ret = simulation(position, num_trials)
+        plt.figure()
+        plt.hist(daily_ret, 100, range=[-1, 1])
+        plt.xlim(-1,1)
+        plt.xlabel('Daily Return')
+        plt.ylabel('Frequency in Number')
+        plt.title('Daily Return of Position{}'.format(position))
+        plt.savefig('histogram_{}_pos.pdf'.format(str(position).zfill(4)))
+        
+        mean = np.mean(daily_ret)
+        std = np.std(daily_ret)
+        results.write('Position: ' + str(position) + '\n')
+        results.write('Mean: ' + str(mean) + '\n')
+        results.write('Standard Deviation: ' + str(std) + '\n')
+        
+    results.close()
+
+if __name__ == '__main__':
+    main()
