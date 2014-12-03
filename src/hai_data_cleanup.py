3036dcea0eb910089c4d6abb73b7bc0f7e0b7e50
pinesol <alpinesol@gmail.com>
2014-12-03 00:33:54 -0500

    Updated parseHAIbyBinLabel to use data_utils.parseFileWithIndex, and added merge_data.py

--- src/hai_data_cleanup.py
+++ src/hai_data_cleanup.py
@@ -12,14 +12,14 @@ from sklearn.metrics import roc_curve, auc
 import data_utils
 
 
-def convertOldHAIDataframe(old_df, year_str):
+def _convertOldHAIDataframe(old_df, year_str):
     '''Replaces the 'Measure' column with the 'Measure ID' column. year_str can be either '2012' or '2013' for now.'''
     # TODO both maps have other HAI_1 measures, like upper and lower bounds
     # TODO 2013_MEASURE_MAP can be extended to have entries for HAI 1 through HAI 4.
     MEASURE_MAP_2013 = {'Central-Line-Associated Blood Stream Infections (CLABSI)': 'HAI_1_SIR',
-    					'CLABSI Compared to National': 'HAI_1_compare'}
+                        'CLABSI Compared to National': 'HAI_1_compare'}
     MEASURE_MAP_2012 = {'Central Line Associated Blood Stream Infections (CLABSI)': 'HAI_1_SIR',
-    					'CLABSI Upper Confidence Limit': 'HAI_1_compare'}
+                        'CLABSI Upper Confidence Limit': 'HAI_1_compare'} # TODO this looks wrong...did jackie work around this?
 
     def measure_map_func(measure_name):
         if year_str == '2013' and measure_name in MEASURE_MAP_2013:
@@ -32,7 +32,7 @@ def convertOldHAIDataframe(old_df, year_str):
     old_df = old_df.drop('Measure', 1)
     return old_df
     
-def categoricalToIndicator(df, compare):
+def _categoricalToIndicator(df, compare):
     """Replaces the string values in a compare column to numerical benchmarks
         -1: Worse than national average
         0: No different than national average
@@ -43,7 +43,7 @@ def categoricalToIndicator(df, compare):
     benchmarks ={benchmarks[0]:1, benchmarks[1]:0, benchmarks[2]:np.nan, benchmarks[3]:-1}
     return df.replace({compare:benchmarks})
 
-def convertToNumeric(df, col_label, missing_marker):
+def _convertToNumeric(df, col_label, missing_marker):
     """Converts missing values denoted by string missing_marker to Nan 
     	and casts column data as float."""
     for marker in missing_marker:
@@ -51,6 +51,7 @@ def convertToNumeric(df, col_label, missing_marker):
     df[col_label] = df[col_label].astype(float)
     return df
 
+# TODO DEPRECATED! doesn't set provider ID as the index.
 def parseHAIFile(filename, year_str):
     '''year_str can be either '2012', '2013', or '2014' for now.'''
     COLUMNS_2014 = ['Provider ID', 'City', 'State', 'ZIP Code', 'County Name', 'Measure ID', 'Score']
@@ -62,11 +63,12 @@ def parseHAIFile(filename, year_str):
     #data = data.set_index(data['Provider ID'])
     #data = data.drop('Provider ID', 1)
     if year_str == '2012' or year_str == '2013':
-        data = convertOldHAIDataframe(data, year_str)
+        data = _convertOldHAIDataframe(data, year_str)
     assert 'Measure ID' in data.columns
     assert 'Measure' not in data.columns
+    # Throws out all rows that don't have the measure ID 'HAI_1_SIR'.
     data = data[data['Measure ID'] == 'HAI_1_SIR']
-    data = convertToNumeric(data, 'Score', ['Not Available', '-'])
+    data = _convertToNumeric(data, 'Score', ['Not Available', '-'])
     # Make Provider ID an integer so we can use it as a merge key. 
     # Other tables have padding zeros, so converting to int standardizes them.
     data['Provider ID'] = data['Provider ID'].astype(int)
@@ -78,21 +80,19 @@ def parseHAIbyBinLabel(filename, year_str):
     Compared to National. -1 means worse than average, 0 no different, 1 better than average.
     Actual SIR value, confidence intervals, etc. will not be included in returned df'''
 
-    COLUMNS_2014 = ['Provider ID', 'City', 'State', 'ZIP Code', 'County Name', 'Measure ID', 'Compared to National']
-    COLUMNS_2013 = ['Provider ID', 'City', 'State', 'ZIP Code', 'County Name', 'Measure', 'Score']
-    COLUMNS_2012 = ['Provider ID', 'City', 'State', 'ZIP Code', 'County Name', 'Measure', 'Score']
+    COLUMNS_2014 = ['City', 'State', 'Measure ID', 'Compared to National']
+    COLUMNS_2013 = ['City', 'State', 'Measure', 'Score']
+    COLUMNS_2012 = ['City', 'State', 'Measure', 'Score']
     useful_columns_map = {'2014': COLUMNS_2014, '2013': COLUMNS_2013, '2012': COLUMNS_2012}
     
-    data = data_utils.parseFile(filename, useful_columns_map[year_str])
-    #data = data.set_index(data['Provider ID'])
-    #data = data.drop('Provider ID', 1)
+    data = data_utils.parseFileWithIndex(filename, useful_columns_map[year_str])
     if year_str == '2013':
-        data = convertOldHAIDataframe(data, year_str)
+        data = _convertOldHAIDataframe(data, year_str)
         data['Compared to National'] = data['Score']
         data = data.drop('Score', 1)
     elif year_str == '2012':
-    	data = binByCI(data, 'CLABSI Lower Confidence Limit', 'CLABSI Upper Confidence Limit', 'Score')
-    	data = convertOldHAIDataframe(data, year_str)
+    	data = _binByCI(data, 'CLABSI Lower Confidence Limit', 'CLABSI Upper Confidence Limit', 'Score')
+    	data = _convertOldHAIDataframe(data, year_str)
     	data = data.drop('Score', 1)
     elif year_str == '2014':
     	data = data.replace({'Measure ID': {'HAI_1_SIR':'HAI_1_compare'}})
@@ -100,24 +100,24 @@ def parseHAIbyBinLabel(filename, year_str):
     assert 'Measure' not in data.columns
     assert 'Compared to National' in data.columns
     assert 'Score' not in data.columns
+    # Throws out all rows that don't have the measure ID 'HAI_1_compare'.
     data = data[data['Measure ID'] == 'HAI_1_compare']
     if year_str !='2012':
-    	data = categoricalToIndicator(data, 'Compared to National')
-    # Make Provider ID an integer so we can use it as a merge key. 
-    # Other tables have padding zeros, so converting to int standardizes them.
-    data['Provider ID'] = data['Provider ID'].astype(int)
+    	data = _categoricalToIndicator(data, 'Compared to National')
+    # Now that we've removed all the rows unrelated to HAI_1_SIR, drop the measure ID.
+    data = data.drop('Measure ID', 1)
     return data
     
-def binByCI(data, lowerLabel, upperLabel, valueLabel):
+def _binByCI(data, lowerLabel, upperLabel, valueLabel):
 	"""For a dataframe with no bin labels, takes the limits of confidence interval
 	and converts them into a numerical indicator, -1, 1, or 0. If the interval
 	contains 1, we will call this no different (i.e. 0)"""
-	hospitals = np.unique(data['Provider ID'])
-	data = convertToNumeric(data, valueLabel, ['Not Available', '-'])
+	hospitals = np.unique(data.index)
+	data = _convertToNumeric(data, valueLabel, ['Not Available', '-'])
 	cp = 'Compared to National'
 	data[cp] = np.nan
 	for h in hospitals:
-		hospital = data.loc[data['Provider ID'] == h]
+		hospital = data.loc[data.index == h]
 		lower = hospital.loc[hospital['Measure'] == lowerLabel, valueLabel]
 		upper = hospital.loc[hospital['Measure'] == upperLabel, valueLabel]
 		lower = lower.get_values()[0]
@@ -131,12 +131,14 @@ def binByCI(data, lowerLabel, upperLabel, valueLabel):
 	return data
 	
 def filterByMeasureID(data, measure_id):
+    # TODO only used by notebook
     data = data.loc[data['Measure ID'] == measure_id]
     # Drop the measure id column, since it's the same for all rows now.
     data = data.drop('Measure ID', 1)
     return data
 
 def removeRowsWithMissingTarget(data, target_column):
+    # TODO only used by notebook
     MISSING_VALUE = 'Not Available'
     data = data[data[target_column] != MISSING_VALUE]
     return data
---
+++ src/merge_data.py
@@ -0,0 +1,70 @@
+
+import sys
+sys.path.append('src/')
+sys.path.append('data/')
+import pandas as pd
+
+import hai_data_cleanup
+
+# TODO: This function hasn't yet been tested outside the 'Merge Data' notebook. 
+# I'm not sure it will be able to find the data files at the moment.
+def mergeHAITables():
+    '''Merges all three HAI tables into one table that's ready to be used by a classifier
+
+    The columns are:
+    'Compared to National 2014', 'City', 'State', 'Compared to National 2013', 'Compared to National 2012'
+    'Compared to National 2014' is the target variable.
+    It's index consists of integers, and is named 'Provider ID'.
+
+    This version uses the bucketed scores from hai_data_cleanup._binByCI. This can/should be
+    changed going forward. The 'Compared to National' columns are the bucketed HAI scores.
+
+    Returns:
+      A dataframe with aforemented columns.
+    '''
+    # It will be indexed by provider ID, have the location data for the hospital, and the HAI scores from all three years.
+
+    # Extract out the 2014 score column, keeping the index. This will be the target column.
+    hai_2014 = hai_data_cleanup.parseHAIbyBinLabel('data/2014/Healthcare Associated Infections - Hospital.csv', '2014')
+    # Removing rows with null target variables
+    hai_2014 = hai_2014[hai_2014['Compared to National'].notnull()]
+    # Renaming target column
+    hai_2014['Compared to National 2014'] = hai_2014['Compared to National']
+    hai_2014.drop('Compared to National', 1)
+    # stripping out everything but the target
+    hai_2014_score = pd.DataFrame(hai_2014['Compared to National 2014'], index=hai_2014.index)
+
+    # Getting the 2012 data, stripping out only the HAI Score, converting the nulls to zeros.
+    hai_2012 = hai_data_cleanup.parseHAIbyBinLabel('data/2012/Healthcare_Associated_Infections.csv', '2012')
+    hai_2012_score = pd.DataFrame(hai_2012['Compared to National'], index=hai_2012.index)
+    # Renaming target column
+    hai_2012_score['Compared to National 2012'] = hai_2012['Compared to National']
+    hai_2012_score = hai_2012_score.drop('Compared to National', 1)
+    # filling in NaNs with zeros
+    hai_2012_score = hai_2012_score.fillna(0)
+
+    # 2013 HAI will keep its HAI score, and the location data. Also filling in the NaNs with zeros.
+    hai_2013 = hai_data_cleanup.parseHAIbyBinLabel('data/2013/Healthcare_Associated_Infections.csv', '2013')
+    hai_2013['Compared to National 2013'] = hai_2013['Compared to National']
+    hai_2013 = hai_2013.drop('Compared to National', 1)
+    hai_2013 = hai_2013.fillna(0) # filling in NaNs with zeros for 2012
+
+    # Creating the final table, join the 2013 location and HAI score data, with the 2012 and 2014 scores.
+    final_table = hai_2014_score
+    final_table = final_table.join(hai_2013, how = 'left')
+    # filling in missing 2013 HAI scores with zeros
+    final_table['Compared to National 2013'] = final_table['Compared to National 2013'].fillna(0)
+    # joining in the 2012 HAI scores
+    final_table = final_table.join(hai_2012_score, how = 'left')
+    # filling in missing 2012 HAI scores with zeros
+    final_table['Compared to National 2012'] = final_table['Compared to National 2012'].fillna(0)
+    
+    # Tests
+    # There should be exactly 2005 rows in the table
+    assert 2005 == len(final_table)
+    # The of hospitals in the final table should equal the set from 2014
+    assert set(hai_2014_score.index) == set(final_table.index)
+    # There shuld be no duplicate provider IDs
+    assert sorted(set(hai_2014_score.index)) == sorted(hai_2014_score.index)
+    
+    return final_table
